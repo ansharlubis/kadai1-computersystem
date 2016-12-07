0405e7b61a688e3a4fe023c1cb035f8b413be5e1
Luthfan Anshar Lubis <lubis.l.aa@m.titech.ac.jp>
2016-12-07 18:31:12 +0900

    plus report

--- sort.c
+++ sort.c
@@ -6,7 +6,7 @@ void sort_int_array(int *ary, int ary_size){
 	int temp;
 
 	for (i = 0; i < ary_size; i++){
-		int k = ary_size;
+		int k = ary_size - 1;
 
 		for (j = 0; j < k; j++){
 			if (ary[j+1] < ary[j]){
@@ -21,8 +21,8 @@ void sort_int_array(int *ary, int ary_size){
 }
 
 int main(){
-	int ary[] = {1, 6, 7, 8, 2, 3, 2, 1};
-	int ary_size = 8;
+	int ary[] = {1, 6, 7, 8, 2, 3, 2, 1, 5, 7, 2, 13, 253, 2, 2, 3, -3, -233, 934, 2, 334, 97};
+	int ary_size = 22;
 
 	sort_int_array(ary, ary_size);
 
